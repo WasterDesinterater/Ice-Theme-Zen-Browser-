09ec761106eedf3c54a80a735cfe8349c85af287
WasterDesinterater <109934978+WasterDesinterater@users.noreply.github.com>
2025-10-26 23:37:36 -0400

    Update tinted-ice.uc.js

--- tinted-ice.uc.js
+++ tinted-ice.uc.js
@@ -1,3 +1,4 @@
+/*This function allows for the stylization of a shadow element within the application menu object. Without it the object would be a stylesheet unchangable, solid color; based on the color of the browser background*/
 function appMenu(){
     async function Stylist(appShadow,appDropdown){
         appShadow[0].setAttribute("style", "background:transparent !important; border:none !important;"); 
