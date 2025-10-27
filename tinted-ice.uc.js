f06316278fad2a5a015e3fd54d15634a16c1fa06
WasterDesinterater <109934978+WasterDesinterater@users.noreply.github.com>
2025-10-27 00:15:24 -0400

    Update tinted-ice.uc.js

--- tinted-ice.uc.js
+++ tinted-ice.uc.js
@@ -1,13 +1,7 @@
 /*This function allows for the stylization of a shadow element within the application menu object. Without it the object would be a stylesheet unchangable, solid color; based on the color of the browser background*/
 function appMenu(){
-    async function Stylist(appShadow,appDropdown){
-        appShadow[0].setAttribute("style", "background:transparent !important; border:none !important;"); 
-    }
-    var appDropdown
-    var appShadow
-    appDropdown=document.getElementById("appMenu-popup");
-    appShadow=appDropdown.shadowRoot.children;
-    Stylist(appShadow,appDropdown);
+    let appDropdown=document.getElementById("appMenu-popup");
+    let appShadow=appDropdown.shadowRoot.children;
+    appShadow[0].setAttribute("style", "background:transparent !important; border:none !important;"); 
 }
-
 document.getElementById("appMenu-popup").addEventListener("popupshowing",appMenu)
